3a0f310d6d5ce64063be8d23627fa107023aed0b
ly <ly1234>
2022-07-22 17:17:36 +0700

    commiut

--- src/components/Banner/FilterFilm/FilterFilm.jsx
+++ src/components/Banner/FilterFilm/FilterFilm.jsx
@@ -1,45 +1,58 @@
-import "./style.css"
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
+import "./style.css";
 import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
 import { useState } from "react";
-import { Input } from "@mui/material";
+import Select from "@mui/material/Select";
 
 const names = ["CANADA & USA", "EUROPE", "SOUTH KOREAN"];
 
 function FilterFilm() {
-  const [age, setAge] = useState("");
+  const [location, setLocation] = useState(0);
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setLocation(event.target.value);
   };
 
   return (
     <div className="bg-filter">
       <div className="filter">
-        <div>
-          {
-            <FormControl sx={{ w: 50 }} hiddenLabel>
-              
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={age}
-                onChange={handleChange}
-                defaultValue="Select Location"
-                variant="outlined"
-                sx={{ color: "none" }}
-              >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
-          }
+        <div className="filter-item">
+          <h5 className="filter-title">Location</h5>
+          <Select
+            style={{
+              width: "100%",
+              height: 30,
+              border: "none",
+              marginTop: 7,
+            }}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={location}
+            onChange={handleChange}
+          >
+            <MenuItem style={{ display: "none" }} value={0}>
+              Select Location
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
         </div>
-      </div> 
+        <div className="filter-item">
+          <h5 className="filter-title">Year</h5>
+        </div>
+        <div className="filter-item">
+          <h5 className="filter-title">Make</h5>
+        </div>
+        <div className="filter-item">
+          <h5 className="filter-title">Model</h5>
+        </div>
+        <div className="filter-item">
+          <h5 className="filter-title">Sub-Model</h5>
+        </div>
+        <div className="filter-item">
+          <h5 className="filter-title">Series</h5>
+        </div>
+      </div>
       <div class="line"></div>
     </div>
   );
